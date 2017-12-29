c7cd87a559ac585740eec88d9fad8ba6e32854d1
shaozucheng <shaozucheng@likingfit.com>
2017-12-29 18:30:40 +0800

    简述：整理代码
    背景信息：
    
    修改内容：

--- app/src/main/java/com/coverflow/MainActivity.java
+++ app/src/main/java/com/coverflow/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextSwitcher;
@@ -23,48 +21,43 @@ public class MainActivity extends AppCompatActivity {
     private CoverFlowAdapter mAdapter;
     private ArrayList<GameEntity> mData = new ArrayList<>(0);
     private TextSwitcher mTitle;
-    FeatureCoverFlow mCoverFlow;
-
+    private FeatureCoverFlow mCoverFlow;
     private TextView mTextView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = findViewById(R.id.jump);
+        mTextView = (TextView) findViewById(R.id.jump);
+        mTitle = (TextSwitcher) findViewById(R.id.title);
+        mCoverFlow = (FeatureCoverFlow) findViewById(R.id.coverflow);
+        initData();
+        setOtherClick();
+    }
 
+    private void initData() {
         mData.add(new GameEntity(R.drawable.image_1, R.string.title1));
         mData.add(new GameEntity(R.drawable.image_2, R.string.title2));
         mData.add(new GameEntity(R.drawable.image_3, R.string.title3));
         mData.add(new GameEntity(R.drawable.image_4, R.string.title4));
-        mData.add(new GameEntity(R.drawable.item2, R.string.title4));
-        mData.add(new GameEntity(R.drawable.item3, R.string.title4));
-        mData.add(new GameEntity(R.drawable.item4, R.string.title4));
-        mData.add(new GameEntity(R.drawable.item5, R.string.title4));
-        mData.add(new GameEntity(R.drawable.item6, R.string.title4));
-
-        mTitle = (TextSwitcher) findViewById(R.id.title);
-        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
-            @Override
-            public View makeView() {
-                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
-                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
-                return textView;
-            }
-        });
+        mData.add(new GameEntity(R.drawable.image_5, R.string.title5));
+        mData.add(new GameEntity(R.drawable.image_6, R.string.title6));
+        mData.add(new GameEntity(R.drawable.image_7, R.string.title7));
+        mData.add(new GameEntity(R.drawable.image_8, R.string.title8));
+        mData.add(new GameEntity(R.drawable.image_9, R.string.title9));
 
         mAdapter = new CoverFlowAdapter(this);
         mAdapter.setData(mData);
-        mCoverFlow = (FeatureCoverFlow) findViewById(R.id.coverflow);
         mCoverFlow.setAdapter(mAdapter);
+        setCoverFlowOnItemClick();
+    }
 
+    private void setCoverFlowOnItemClick() {
         mCoverFlow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ImageView imageView = view.findViewById(R.id.image);
                 if (imageView != null) {
-                   // mCoverFlow.scrollToPosition();
                     GameEntity entity = (GameEntity) imageView.getTag();
                     Toast.makeText(MainActivity.this, entity.titleResId, Toast.LENGTH_SHORT).show();
                 }
@@ -82,8 +75,17 @@ public class MainActivity extends AppCompatActivity {
                 mTitle.setText("");
             }
         });
+    }
 
-
+    private void setOtherClick() {
+        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
+                return textView;
+            }
+        });
         mTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
+
 
 }
